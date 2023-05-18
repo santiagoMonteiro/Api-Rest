9f389ab50680be1301b0ce44676efd25f9b9dafa
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-05-18 17:52:57 -0400

    Create Transaction Route & Knex integrated with TS

---
+++ src/routes/transactions.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+import { z } from 'zod'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    const transaction = {
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    }
+
+    await knex('transactions').insert(transaction)
+
+    return reply.status(201).send()
+  })
+}
