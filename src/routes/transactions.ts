94d6465a65408960435f3aeed9e56864b9df6b89
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-05-18 20:22:18 -0400

    get transaction & list transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,28 @@ import { knex } from '../database'
 import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
